5c10196d519d9fabb656a16177141a7770da0142
Ana Julia <aj9754747@gmail.com>
2025-10-05 12:04:06 -0300

    Create Recompensas.java

---
+++ src/model/Recompensas.java
@@ -0,0 +1,69 @@
+package model;
+
+public class Recompensa {
+    private int idBonus;
+    private int idUsuario;
+    private String descricao;
+    private double valor;
+    private String status;
+
+    public Recompensa() {
+    }
+
+    public Recompensa(int idBonus, int idUsuario, String descricao, double valor, String status) {
+        this.idBonus = idBonus;
+        this.idUsuario = idUsuario;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.status = status;
+    }
+
+    public int getIdBonus() {
+        return this.idBonus;
+    }
+
+    public void setIdBonus(int idBonus) {
+        this.idBonus = idBonus;
+    }
+
+    public int getIdUsuario() {
+        return this.idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Recompensa #" + this.idBonus);
+        System.out.println("Usuário ID: " + this.idUsuario);
+        System.out.println("Descrição: " + this.descricao);
+        System.out.println("Valor: " + this.valor);
+        System.out.println("Status: " + this.status);
+        System.out.println("--------------------------------");
+    }
+}
