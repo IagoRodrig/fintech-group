04b29c6c2189407614ec3ee9ecf0d912e1b91d40
Ana Julia <aj9754747@gmail.com>
2025-10-05 12:03:06 -0300

    Create Bonus.java

---
+++ src/model/Bonus.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Bonus extends Recompensa {
+    private String tipoBonus;
+
+    public Bonus(int idBonus, int idUsuario, String descricao, double valor, String status, String tipoBonus) {
+        super(idBonus, idUsuario, descricao, valor, status);
+        this.tipoBonus = tipoBonus;
+    }
+
+    public String getTipoBonus() {
+        return this.tipoBonus;
+    }
+
+    public void setTipoBonus(String tipoBonus) {
+        this.tipoBonus = tipoBonus;
+    }
+
+    public void exibirDetalhes() {
+        super.exibirDetalhes();
+        System.out.println("Tipo de bônus: " + this.tipoBonus);
+        System.out.println("================================");
+    }
+}
